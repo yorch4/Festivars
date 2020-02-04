c883e82eaaa1fd10fe0715cbf3276a322b19c71d
yorch4 <jorge.rgdaw@gmail.com>
2020-02-04 14:17:44 +0100

    ''

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBfe6bo1d4ZyOlFwNujCVW3Pbg4xMjDKGQ",
+    authDomain: "festivars.firebaseapp.com",
+    databaseURL: "https://festivars.firebaseio.com",
+    projectId: "festivars",
+    storageBucket: "festivars.appspot.com",
+    messagingSenderId: "481130196318",
+    appId: "1:481130196318:web:5a48a489a583e0fd9073cf",
+    measurementId: "G-MXYHVPGZ5F"
+  }
 };
 
 /*
